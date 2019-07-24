688209d8fef73385c7122005574af6093d1e3bde
lucasl@oktana.io <lucasl@oktana.io>
2019-07-24 12:18:32 -0300

    Basic ABM Account

---
+++ Pappelitos.BusinessLogic.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Pappelitos.BusinessLogic.Test
+{
+    public class Class1
+    {
+    }
+}
--- Pappelitos.BusinessLogic/AccountLogic.cs
+++ Pappelitos.BusinessLogic/AccountLogic.cs
@@ -1,8 +1,34 @@
 ﻿using System;
+using Pappelitos.Domain;
+using Pappelitos.BusinessLogic.Interface;
+using Pappelitos.DataAccess.Interface;
 
 namespace Pappelitos.BusinessLogic
 {
-    public class AccountLogic
+    public class AccountLogic : IAccountLogic
     {
+        private IRepository<Account> accountRepository;
+        public AccountLogic(IRepository<Account> repository)
+        {
+            this.accountRepository = repository;
+        }
+        public Account AddAccount(Account account)
+        {
+            accountRepository.Add(account);
+            accountRepository.Save();
+            return account;
+        }
+        public Account DeleteAccount(Account account)
+        {
+            accountRepository.Delete(account);
+            accountRepository.Save();
+            return account;
+        }
+        public Account UpdateAccount(Account account)
+        {
+            accountRepository.Update(account);
+            accountRepository.Save();
+            return account;
+        }
     }
 }
